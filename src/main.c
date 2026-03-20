80875bed16e6435ac8ce2fdfb0e272fc9456c25e
Alexander Moeller <moeller.alexander@proton.me>
2026-03-19 22:38:02 -0500

    Add while loop for number entry.

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 
 
 int main() {
-    long num = 0;
+    long long num = 0;
     int bits = 0;
     long long max = 0;
     char check;
@@ -16,26 +16,27 @@ int main() {
 
     max = 1LL << (bits - 1);
 
-    printf("Enter a decimal number (%lli to %lli): ", -max, max-1);
-
-    if(scanf("%ld%c", &num, &check) != 2 || check != '\n'){
-        printf("Error: Invalid input. Please enter a whole number only.\n");
-        
-        while (getchar() != '\n');
-        return 1;
+    while(1){
+    check = 0;
+    printf("Enter a whole number (%lld to %lld): \n", -max, max-1);
+        if(scanf("%lld%c", &num, &check) == 2 && check == '\n'){
+            if(num < max && num >= -max) break;
+            else{
+                printf("Error: Number is out of range!\n");
+                continue;
+            }
+        }
+        printf("Error! Invalid input.\n");
+        if(check != '\n'){
+            int c;
+            while((c = getchar()) != '\n' && c != EOF);
+        }
     }
 
-    if(num >= max){
-        printf("Invalid input!");
-        return 1;
-    }
+    printf("Binary equivalent of %lld is: ", num);
 
-    printf("Binary equivalent of %ld is: ", num);
-
-    // We iterate from the most significant bit (2^7 = 128) 
-    // down to the least significant bit (2^0 = 1).
     for (int i = bits - 1; i >= 0; i--) {
-        printf("%ld", (num >> i) & 1);
+        printf("%lld", (num >> i) & 1);
 
         if(i % 4 == 0 && i != 0) printf(" ");
     }
