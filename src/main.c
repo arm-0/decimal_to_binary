d8500b16069b98f05f0aafe545496b02e7507911
Alexander Moeller <moeller.alexander@proton.me>
2026-03-19 21:47:59 -0500

    Expanded for up to 31 bits and 2's complement.

--- src/main.c
+++ src/main.c
@@ -1,21 +1,43 @@
 #include <stdio.h>
 
-int main() {
-    int decimal = 0;
-    printf("Enter a decimal number (0-255): ");
-    scanf("%d", &decimal);
 
-    if(decimal > 255 || decimal < 0){
-        printf("Invalid input!");
-        return 0;
+int main() {
+    long num = 0;
+    int bits = 0;
+    long long max = 0;
+    char check;
+
+    printf("How many bits? E.g., 2, 8, 16, ... , 31.\n");
+    if(scanf("%d%c", &bits, &check) != 2 || check != '\n' || bits <= 0 || bits >= 32){
+        printf("Error: Invalid bit selection!\n");
+        while(check != '\n' && getchar() != '\n');
+        return 1;
+    } 
+
+    max = 1LL << (bits - 1);
+
+    printf("Enter a decimal number (%lli to %lli): ", -max, max-1);
+
+    if(scanf("%ld%c", &num, &check) != 2 || check != '\n'){
+        printf("Error: Invalid input. Please enter a whole number only.\n");
+        
+        while (getchar() != '\n');
+        return 1;
     }
 
-    printf("Binary equivalent of %d is: ", decimal);
+    if(num >= max){
+        printf("Invalid input!");
+        return 1;
+    }
+
+    printf("Binary equivalent of %ld is: ", num);
 
     // We iterate from the most significant bit (2^7 = 128) 
     // down to the least significant bit (2^0 = 1).
-    for (int i = 7; i >= 0; i--) {
-        printf("%d", (decimal >> i) & 1);
+    for (int i = bits - 1; i >= 0; i--) {
+        printf("%ld", (num >> i) & 1);
+
+        if(i % 4 == 0 && i != 0) printf(" ");
     }
 
     printf("\n");
